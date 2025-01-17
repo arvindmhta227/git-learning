a1245902ba87539dc2829e514cb9c91719ebe150
Arvind <mehtabdal2004@gmail.com>
2025-01-17 19:30:34 +0530

    pay first commit

--- first.js
+++ first.js
@@ -1,9 +1,13 @@
 const name = "arvind kumar";
-const age = 19;
+const age = 90;
 const balance = 1234;
 
 const first_master = "my first commit "
 const first_masters = "my first commit "
 const first_feature = "my first feature commit"
-const second_feature = "my second feature commit"
+const second_features = "my second feature commit"
 console.log("merge succesfull");
+const beast = "my first feature commit"
+const second_feature = "my second feature commit"
+const first_bug = "my first bug commit"
+console.log("mege them");
